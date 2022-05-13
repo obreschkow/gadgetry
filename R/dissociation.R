8c6675b36e4b6c9db98eb193b75976dba7108ead
obreschkow <danail.obreschkow@gmail.com>
2022-05-13 16:56:27 +0800

    dissociation with 2D estimator

--- R/dissociation.R
+++ R/dissociation.R
@@ -4,8 +4,8 @@
 #'
 #' @description Evaluates the multipole moment differences of two 3D point sets; designed to study large-scale dissociation between dark matter and gas in simulations.
 #'
-#' @param x n-by-3 matrix containing the first set of points (e.g. dark matter)
-#' @param y m-by-3 matrix containing the second set of points (e.g. gas)
+#' @param x n-by-3 matrix containing the first set of points (e.g. dark matter). It can also be a n-by-2 matrix, if the line-of-sight coordinate is missing (as in observations). In this case, a correction is applied to estimate the 3D dissociation indices.
+#' @param y m-by-3 matrix containing the second set of points (e.g. gas). It can also be a m-by-2 matrix, if the line-of-sight coordinate is missing (as in observations). In this case, a correction is applied to estimate the 3D dissociation indices.
 #'
 #' @return list of dissociation indices and moments
 #'
@@ -17,8 +17,8 @@
 #' x.cdm = t(cbind(t(cooltools::runif3(5e3))-c(1,0,0),t(cooltools::runif3(5e3))+c(1,0,0)))
 #' cat(sprintf('Primary dissociation index = %.2f\n',dissociation(x.cdm,x.gas)$D2))
 #' \dontrun{
-#' dat = list(PartType0 = list(Coordinates=x.gas), PartType1 = list(Coordinates=x.cdm))
-#' plot(dat)
+#' sn = snapshot(list(x.gas,x.cdm))
+#' plot(sn)
 #' }
 #'
 #' @export
@@ -29,8 +29,14 @@ dissociation = function(x,y) {
   # ensure that input is always a matrix
   x = as.matrix(x)
   y = as.matrix(y)
-  if (length(x)==3) x = array(x,c(1,3))
-  if (length(y)==3) y = array(y,c(1,3))
+
+  # check if coordinates are 2D or 3D
+  d = dim(x)[2]
+  if (dim(y)[2]!=d) stop('x and y must have the same dimension')
+  if (d==2) {
+    x = cbind(x,0)
+    y = cbind(y,0)
+  }
 
   # number index
   nx = dim(x)[1]
@@ -60,6 +66,10 @@ dissociation = function(x,y) {
       for (i in seq(2*l+1)) {
         fx[i] = mean(cooltools::sphericalharmonics(l,m[i],x)*rx)
         fy[i] = mean(cooltools::sphericalharmonics(l,m[i],y)*ry)
+      }
+      if (d==2) {
+        fx[l+1] = 0
+        fy[l+1] = 0
       }
       prefactor = sqrt(4*pi/(2*l+1))
       kx = sqrt(sum(abs(fx)^2))
