45b286007ca40ced5c7612b93592654050a7366f
obreschkow <danail.obreschkow@gmail.com>
2023-05-24 09:47:54 +0800

    updated snapshot interpolation

--- R/snapshotinterp.R
+++ R/snapshotinterp.R
@@ -4,12 +4,12 @@
 #'
 #' @param sn0 first gadget snapshot
 #' @param sn1 second gadget snapshot
-#' @param t0 time of first snapshot
-#' @param t1 time of second snapshot
-#' @param ti time of interpolated output snapshot. Values outside the range [t0,t1] lead to an extrapolation.
+#' @param f interpolation time: 0 corresponds to time t0, 1 to time t1; should be within [0,1]
+#' @param t0 time of first snapshot, if not given it is extracted from the header of sn0
+#' @param t1 time of second snapshot, if not given it is extracted from the header of sn1
 #' @param usevelocities logical flag. If TRUE, velocities are used to improve the interpolation of positions.
 #' @param v0factor factor to be applied to velocities of sn0 to convert to length/time units of the snapshot coordinates and the t0/t1/ti times.
-#' @param v1factor factor to be applied to velocities of sn1 to convert to length/time units of the snapshot coordinates and the t0/t1/ti times. If not provided, this will be set equal to \code{v1factor}.
+#' @param v1factor factor to be applied to velocities of sn1 to convert to length/time units of the snapshot coordinates and the t0/t1/ti times.
 #' @param afield optional acceleration field for leapfrog integration. This must be a function of a n-by-3 array representing n position vectors, which returns an n-by-3 array with the n acceleration vectors in the same length and time units as the snapshot coordinates and t0/t1/ti times.
 #' @param dt optional time step used for leapfrog integration; only used if \code{afield} given. The default is \code{dt=abs(t1-t0)/20}.
 #'
@@ -19,113 +19,68 @@
 #'
 #' @export
 
-snapshotinterp = function(sn0,sn1,t0=0,t1=1,ti=0.5,
-                          usevelocities=TRUE,v0factor=1,v1factor=NULL,
-                          afield=NULL,dt=NULL) {
+snapshotinterp = function(sn0, sn1, f=0.5, t0=NULL, t1=NULL,
+                          usevelocities=TRUE, v0factor=1, v1factor=v0factor,
+                          afield=NULL, dt=NULL) {
 
-  if (is.null(v1factor)) v1factor=v0factor
-
-  if (ti==t0) {
+  if (f==0) {
 
     sni = sn0
 
-  } else if (ti==t1) {
+  } else if (f==1) {
 
     sni = sn1
 
   } else {
 
-    # copy properties from nearest snapshot
-    f = max(0,min(1,(ti-t0)/(t1-t0)))
-    if (f<0.5) {
-      master = 0
-      slave = 1
-      sni = sn0
-    } else {
-      master = 1
-      slave = 0
-      sni = sn1
+    # get times
+    if (is.null(t0)) {
+      if (is.null(sn0$Header$Time)) {
+        stop('sn0$Header$Time missing; provide value of t0')
+      } else {
+        t0 = sn0$Header$Time
+      }
+    }
+    if (is.null(t1)) {
+      if (is.null(sn1$Header$Time)) {
+        stop('sn1$Header$Time missing; provide value of t1')
+      } else {
+        t1 = sn1$Header$Time
+      }
     }
 
-    # sort particles of other snapshot in same order
-    idmaster = allpart(sni,'ParticleIDs')
-    if (slave==0) {
-      idslave = allpart(sn0,'ParticleIDs')
-    } else {
-      idslave = allpart(sn1,'ParticleIDs')
-    }
+    # make master snapshot (sn[[master]]) and slave snapshot (sn[[slave]]), such that the interpolated sn is closer to the master
+    sn = list(sn0,sn1)
+    sn0 = sn1 = NULL
+    master = ifelse(f<0.5,1,2)
+    slave = 3-master
 
-    k = sort.int(sort.int(idmaster,index.return=TRUE)$ix,index.return=TRUE)$ix
-    indexslave = sort.int(idslave,index.return=TRUE)$ix[k]
+    # copy master snapshot to interpolated snapshot
+    sni = sn[[master]]
+
+    # sort particles of slave in order of master
+    idmaster = allpart(sn[[master]],'ParticleIDs')
+    idslave = allpart(sn[[slave]],'ParticleIDs')
+    indexslave = order(idslave)[idmaster]
     if (any(idmaster!=idslave[indexslave])) stop('the two snapshots do not contain the same particle IDs')
 
-    # determine extra fields
-    fieldnames = c()
-    for (species in seq(0,10)) {
-      field = sprintf('PartType%d',species)
-      fieldnames = c(fieldnames,names(sn0[[field]]),names(sn1[[field]]))
-    }
-    fieldnames = unique(fieldnames)
-    fieldnames = fieldnames[!fieldnames%in%c('Coordinates','Velocities','ParticleIDs')]
-
-    # complete missing masses and internal energies
-    if ('Masses'%in%fieldnames) {
-      for (species in seq(0,10)) {
-        field = sprintf('PartType%d',species)
-        if (!is.null(sni[[field]])) {
-          if (is.null(sn0[[field]]$Masses)) sn0[[field]]$Masses=rep(NA,dim(sn0[[field]]$Coordinates)[1])
-          if (is.null(sn1[[field]]$Masses)) sn1[[field]]$Masses=rep(NA,dim(sn1[[field]]$Coordinates)[1])
-        }
-      }
-    }
-    if ('InternalEnergy'%in%fieldnames) {
-      for (species in seq(0,10)) {
-        field = sprintf('PartType%d',species)
-        if (!is.null(sni[[field]])) {
-          if (is.null(sn0[[field]]$InternalEnergy)) sn0[[field]]$InternalEnergy=rep(NA,dim(sn0[[field]]$Coordinates)[1])
-          if (is.null(sn1[[field]]$InternalEnergy)) sn1[[field]]$InternalEnergy=rep(NA,dim(sn1[[field]]$Coordinates)[1])
-        }
-      }
-    }
-
-    # extract ordered positions and velocities
-    if (master==0) {
-      x0 = allpart(sn0,'Coordinates')
-      x1 = allpart(sn1,'Coordinates')[indexslave,]
-      v0 = allpart(sn0,'Velocities')*v0factor
-      v1 = allpart(sn1,'Velocities')[indexslave,]*v1factor
-      if ('Masses'%in%fieldnames) {
-        m0 = allpart(sn0,'Masses')
-        m1 = allpart(sn1,'Masses')[indexslave]
-      }
-      if ('InternalEnergy'%in%fieldnames) {
-        u0 = allpart(sn0,'InternalEnergy')
-        u1 = allpart(sn1,'InternalEnergy')[indexslave]
-      }
+    # extract coordinates and velocities into simple arrays
+    if (master==1) {
+      x0 = allpart(sn[[1]],'Coordinates')
+      x1 = allpart(sn[[2]],'Coordinates')[indexslave,]
+      v0 = allpart(sn[[1]],'Velocities')*v0factor
+      v1 = allpart(sn[[2]],'Velocities')[indexslave,]*v1factor
     } else {
-      x0 = allpart(sn0,'Coordinates')[indexslave,]
-      x1 = allpart(sn1,'Coordinates')
-      v0 = allpart(sn0,'Velocities')[indexslave,]*v0factor
-      v1 = allpart(sn1,'Velocities')*v1factor
-      if ('Masses'%in%fieldnames) {
-        m0 = allpart(sn0,'Masses')[indexslave]
-        m1 = allpart(sn1,'Masses')
-      }
-      if ('InternalEnergy'%in%fieldnames) {
-        u0 = allpart(sn0,'InternalEnergy')[indexslave]
-        u1 = allpart(sn1,'InternalEnergy')
-      }
+      x0 = allpart(sn[[1]],'Coordinates')[indexslave,]
+      x1 = allpart(sn[[2]],'Coordinates')
+      v0 = allpart(sn[[1]],'Velocities')[indexslave,]*v0factor
+      v1 = allpart(sn[[2]],'Velocities')*v1factor
     }
-    if ('Masses'%in%fieldnames) {
-      m0[is.na(m0)] = m1[is.na(m0)]
-      m1[is.na(m1)] = m0[is.na(m1)]
-    }
-    if ('InternalEnergy'%in%fieldnames) {
-      u0[is.na(u0)] = u1[is.na(u0)]
-      u1[is.na(u1)] = u0[is.na(u1)]
-    }
+    if (length(unique(c(dim(x0)[1],dim(x1)[1],dim(v0)[1],dim(v1)[1])))!=1) stop('Missing coordinates/velocities')
 
     # interpolate positions & velocities
+    ti = t0*(1-f)+t1*f
+    sni$Header$Time = ti
     if (usevelocities) {
       p = particleinterp(x0=x0,x1=x1,t0=t0,t1=t1,ti=ti,v0=v0,v1=v1,afield=afield,dt=dt)
     } else {
@@ -153,20 +108,12 @@ snapshotinterp = function(sn0,sn1,t0=0,t1=1,ti=0.5,
         }
         sni[[field]]$Coordinates = p$x[n+seq(ntype),]
         sni[[field]]$Velocities = p$v[n+seq(ntype),]
-        if ('Masses'%in%fieldnames) {
-          sni[[field]]$Masses = (1-f)*m0[n+seq(ntype)]+f*m1[n+seq(ntype)]
-        }
-        if ('InternalEnergy'%in%fieldnames) {
-          sni[[field]]$InternalEnergy = exp((1-f)*log(u0[n+seq(ntype)])+f*log(u1[n+seq(ntype)]))
-        }
         n = n+ntype
       }
     }
     if (n!=dim(p$x)[1]) stop('unidentified interpolation indexing error')
 
   }
-
-  sni$Header$Time = ti
 
   class(sni) = "snapshot"
   return(sni)
